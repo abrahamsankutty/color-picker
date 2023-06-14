d38c600d8c7dbd1a07fa533346995f24cb0f09a1
Abraham Sankutty <abrahamsankutty@gmail.com>
2023-06-14 14:16:34 +0530

    Update

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -2,7 +2,38 @@ import { useState } from "react";
 
 export default function Palette() {
   const [palette, setPalette] = useState(["black", "red", "green", "yellow"]);
-  setPalette;
+
+  const [color, setColor] = useState("blue");
+
+  const getRgb = () => Math.floor(Math.random() * 256);
+
+  const rgbToHex = (r: any, g: any, b: any) =>
+    "#" +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      })
+      .join("");
+
+  const handleGenerate = () => {
+    const color = {
+      r: getRgb(),
+      g: getRgb(),
+      b: getRgb(),
+    };
+
+    setColor(rgbToHex(color.r, color.g, color.b));
+  };
+
+  function HandleIncrease() {
+    handleGenerate();
+    console.log(color);
+    setPalette((palette) => [...palette, color]);
+  }
+  function HandleDecrease() {
+    setPalette((palette) => palette.slice(0, -1));
+  }
   return (
     <>
       <div className="ncp-row">
@@ -10,7 +41,7 @@ export default function Palette() {
           {palette.map((palette) => {
             return (
               <div
-                key="palette"
+                key={palette}
                 className="ncp-palette"
                 style={{
                   backgroundColor: palette,
@@ -20,8 +51,12 @@ export default function Palette() {
           })}
         </div>
         <div className="ncp-update-palette">
-          <div className="ncp-increase">+</div>
-          <div className="ncp-decrease">-</div>
+          <div className="ncp-increase" onClick={HandleIncrease}>
+            +
+          </div>
+          <div className="ncp-decrease" onClick={HandleDecrease}>
+            -
+          </div>
         </div>
       </div>
     </>
