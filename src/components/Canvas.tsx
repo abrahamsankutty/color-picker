1b338d27867867f5fe3cf1f01641f0f7779aa5ec
Abraham Sankutty <abrahamsankutty@gmail.com>
2023-06-16 06:44:40 +0530

    Update

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Draggable from "react-draggable";
 export default function Canvas(data: any) {
+  const [deltaPosition, setDeltaPosition] = useState([0, 0]);
+  const [bgColor, setBGColor] = useState([255, 255, 255, 255]);
   const dragHandlers = {};
+  const canvas: any = document.getElementById("mycanvas");
+  const ctx = canvas?.getContext("2d");
+
   useEffect(() => {
     const canvas: any = document.getElementById("mycanvas");
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas?.getContext("2d");
     const image = new Image();
+    image.setAttribute("crossOrigin", "");
     image.src = data.path || "https://picsum.photos/200";
 
     image.onload = () => {
       ctx.drawImage(image, 0, 0, 500, 500);
     };
   }, [data]);
+
+  const handleDrag = (e: any, ui: any) => {
+    setDeltaPosition([ui.x, ui.y]);
+    e;
+    deltaPosition;
+    setBGColor(ctx.getImageData(ui.x + 13, ui.y + 13, 3, 3).data);
+  };
+
   return (
     <>
       <div
@@ -21,12 +35,13 @@ export default function Canvas(data: any) {
           position: "relative",
         }}
       >
-        <Draggable bounds="parent" {...dragHandlers}>
+        <Draggable bounds="parent" onDrag={handleDrag} {...dragHandlers}>
           <div
             className="ncp-color-picker"
             style={{
               top: "0px",
               left: "0px",
+              backgroundColor: `rgb(${bgColor[0]},${bgColor[1]},${bgColor[2]})`,
             }}
           ></div>
         </Draggable>
